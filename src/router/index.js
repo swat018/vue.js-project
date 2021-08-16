5b05fff44202347d4377697d4da8118e6205f595
jinwoo park <swat018@gmail.com>
2021-08-16 21:35:06 +0900

    데이터 바인딩 - 이벤트 처리(v-on):Change 이벤트

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import {
   createWebHistory
 } from 'vue-router'
 import Home from '../views/Home.vue'
-import  EventClick3 from '../views/EventClick3.vue'
+import  EventChange from '../views/EventChange.vue'
 
 const routes = [
   {
@@ -20,9 +20,9 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
-    path: '/eventclick3',
-    name: 'EventClick3',
-    component: EventClick3
+    path: '/eventchange',
+    name: 'EventChange',
+    component: EventChange
   }
 ]
 
---
+++ src/views/EventChange.vue
@@ -0,0 +1,23 @@
+<template>
+  <select v-model="selectValue" @change="changeSelect">
+    <option value="서울">서울</option>
+    <option value="부산">부산</option>
+    <option value="제주">제주</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "EventChange",
+  data() {
+    return {
+      selectValue: ''
+    };
+  },
+  methods: {
+    changeSelect(){
+      alert(this.selectValue);
+    }
+  }
+}
+</script>
