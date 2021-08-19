163a88e68b4341d34a9ad429da84d747d2267eca
jinwoo park <swat018@gmail.com>
2021-08-19 23:52:27 +0900

    (컴포넌트 심화) Slot

---
+++ src/views/SlotUseModalLayout.vue
@@ -0,0 +1,12 @@
+<modal-layout>
+  <template v-slot:header>
+    <h1>팝업 타이틀</h1>
+  </template>
+  <template v-slot:default>
+    <p>팝업 컨텐츠</p>
+    <P>팝업 컨텐츠</P>
+  </template>
+  <template v-slot:footer>
+    <button type="button">닫기</button>
+  </template>
+</modal-layout>
