5b913ce04780353c8714bd2b1bd07b6feadd3223
jinwoo park <swat018@gmail.com>
2021-08-22 22:50:50 +0900

    Reusability & Composition - CompositionAPI(CompositionAPI4)

---
+++ src/views/CompositionAPI4.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>Calculator</h2>
+    <div>
+      <input type="text" v-model="num1" />
+      <span> + </span>
+      <input type="text" v-model="num2" />
+      <sapn> = </sapn>
+      <span>{{result}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import {plusCalculator} from '../common.js'
+export default {
+  name: "CompositionAPI4",
+  setup() {
+    let {num1, num2, result} = plusCalculator();
+    return {
+      num1, num2, result
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
