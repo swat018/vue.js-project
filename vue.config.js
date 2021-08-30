c329383cc06aa51bd7f24818cb3d2ae9817e7bc4
jinwoo park <swat018@gmail.com>
2021-08-31 00:12:06 +0900

    미니프로로젝트 : 로그인 처리 (네이버 아이디로 로그인하기) - proxy 등록 추가

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@
 module.exports = {
     chainWebpack: config => {
         config.plugins.delete('prefetch');  // prefetch 삭제
-    }
+    },
     // devServer: {
     //     port: 8080,
     //     proxy: {
@@ -14,4 +14,11 @@ module.exports = {
     //         }
     //     }
     // }
+    devServer: {
+        proxy: {
+            '/oauth2.0': {
+                target: 'https://nid.naver.com'
+            }
+        }
+    }
 }
